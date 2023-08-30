0ce137a5ed29652e449e57effaf59e56ee735b77
Pedro Cruz <pedro.cruz.lopez001@gmail.com>
2023-08-30 11:11:06 -0500

    se crearon las clases necesarias para el pryecto

---
+++ src/model/Disk.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Disk {
+    private int height;
+    private int width;
+
+    public Disk(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+}
+
