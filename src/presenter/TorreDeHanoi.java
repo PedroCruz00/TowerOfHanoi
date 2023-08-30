af0bad4a6ba9734757bdd851e8bc070fe071424e
Pedro Cruz <pedro.cruz.lopez001@gmail.com>
2023-08-30 16:44:13 -0500

    Se crea el presenter con el run para correr el programa

--- src/presenter/TorreDeHanoi.java
+++ src/presenter/TorreDeHanoi.java
@@ -1,6 +1,23 @@
 package presenter;
 
+
+import model.Disk;
+import view.View;
+
 public class TorreDeHanoi {
 
+    private View view;
+    private Disk disk;
 
+    public TorreDeHanoi() {
+        view = new View(25,350);
+        run();
+    }
+
+    private void run(){
+        view.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new TorreDeHanoi();
+    }
 }
