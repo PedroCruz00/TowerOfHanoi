b72e37e2184d163dc751b5dce242b75da86e2122
Pedro Cruz <pedro.cruz.lopez001@gmail.com>
2023-08-30 15:57:08 -0500

    Se crea el menu para ingresaar el numero de discos

--- src/view/InitMessage.java
+++ src/view/InitMessage.java
@@ -1,8 +1,20 @@
 package view;
 
-
+import javax.swing.*;
+import java.awt.*;
 
 public class InitMessage {
-
-
+    public int readDiskNumber(Component component,int diskNumber) {
+        try {
+            diskNumber = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero de discos"));
+            if (diskNumber < 1 || diskNumber > 18) {
+                JOptionPane.showMessageDialog(component, "El numero de discos debe ser %numeroDiscos% >= 2 y %numeroDiscos% <= 50".replaceAll("%numeroDiscos%", String.valueOf(diskNumber)));
+                readDiskNumber(component,diskNumber);
+            }
+        } catch (HeadlessException|NumberFormatException e) {
+            JOptionPane.showMessageDialog(component, "Ingrese solo numeros enteros");
+            readDiskNumber(component,diskNumber);
+        }
+        return diskNumber;
+    }
 }
